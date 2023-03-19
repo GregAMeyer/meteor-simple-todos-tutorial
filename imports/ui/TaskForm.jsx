9ffd11326f70a3839375ac05f93169de483a169e
GregAMeyer <gregmeyer888@gmail.com>
2023-03-19 15:25:29 -0400

    move db logic files to db directory

--- imports/ui/TaskForm.jsx
+++ imports/ui/TaskForm.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
-import { TasksCollection } from '/imports/api/TasksCollection';
+import { TasksCollection } from '/imports/db/TasksCollection';
 
-export const TaskForm = ({user}) => {
+export const TaskForm = () => {
   const [text, setText] = useState("");
 
   const handleSubmit = e => {
